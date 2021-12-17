17817843b9bbc28dc97d4dac0293d3c7934b6128
Grig Alex <alexengrigdev@gmail.com>
2021-12-18 01:30:52 +0300

    Create NotOverridableMethodsDomain

---
+++ integration-test/src/main/java/dev/alexengrig/util/annotation/unmodifiablewrapper/NotOverridableMethodsDomain.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.util.annotation.unmodifiablewrapper;
+
+import dev.alexengrig.util.annotation.UnmodifiableWrapper;
+
+@UnmodifiableWrapper
+public class NotOverridableMethodsDomain {
+
+    private int integer;
+
+    public static void staticMethod() {
+        // do nothing
+    }
+
+    public void set(Object o) {
+        // do nothing
+    }
+
+    public int getInteger() {
+        return integer;
+    }
+
+    public void setInteger(int integer) {
+        this.integer = integer;
+    }
+
+    private int getPrivateInteger() {
+        return integer;
+    }
+
+    private void setPrivateInteger(int integer) {
+        this.integer = integer;
+    }
+
+    public final int getFinalInteger() {
+        return integer;
+    }
+
+    public final void setFinalInteger(int integer) {
+        this.integer = integer;
+    }
+
+}
---
+++ integration-test/src/test/java/dev/alexengrig/util/annotation/unmodifiablewrapper/NotOverridableMethodsDomainTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.alexengrig.util.annotation.unmodifiablewrapper;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NotOverridableMethodsDomainTest {
+
+    @Test
+    void testForMethods() {
+        NotOverridableMethodsDomain domain = new NotOverridableMethodsDomain();
+        UnmodifiableNotOverridableMethodsDomain wrapper = new UnmodifiableNotOverridableMethodsDomain(domain);
+        assertNotNull(wrapper);
+        Method[] methods = UnmodifiableNotOverridableMethodsDomain.class.getDeclaredMethods();
+        Map<String, Method> methodByName = Arrays.stream(methods)
+                .collect(Collectors.toMap(Method::getName, Function.identity()));
+        assertTrue(methodByName.containsKey("set"));
+        assertTrue(methodByName.containsKey("getInteger"));
+        assertTrue(methodByName.containsKey("setInteger"));
+        assertFalse(methodByName.containsKey("staticMethod"));
+        assertFalse(methodByName.containsKey("getPrivateInteger"));
+        assertFalse(methodByName.containsKey("setPrivateInteger"));
+        assertFalse(methodByName.containsKey("getFinalInteger"));
+        assertFalse(methodByName.containsKey("setFinalInteger"));
+    }
+
+}
